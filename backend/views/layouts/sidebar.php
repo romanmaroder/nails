8e31c86b2c0806f2e28b007555105a6fd81eaba0
romanmaroder <kashik.roman@rambler.ru>
2021-09-01 14:38:16 +0300

    Logo picture

--- backend/views/layouts/sidebar.php
+++ backend/views/layouts/sidebar.php
@@ -3,10 +3,6 @@
 use common\models\Event as EventAlias;
 use common\models\User;
 use hail812\adminlte\widgets\Menu;
-
-/*@var $assetDir backend\views\layouts\main */
-/*@var $countEventTotal backend\views\layouts\main */
-//$assetDir                      = Yii::$app->assetManager->getPublishedUrl('@vendor/almasaeed2010/adminlte/dist');
 ?>
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
 	<!-- Brand Logo -->
@@ -14,7 +10,8 @@ use hail812\adminlte\widgets\Menu;
 	   title="На сайт"
 	   data-toggle="tooltip"
 	   data-placement="auto">
-		<img src="<?= $assetDir ?>/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+		<img src="/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image
+		img-circle elevation-3"
 			 style="opacity: .8">
 		<span class="brand-text font-weight-light"><?php
             echo Yii::$app->name; ?></span>
