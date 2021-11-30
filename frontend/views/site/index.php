8d0b20351b8a3042f90895d2a7f3dd30e12888a4
romanmaroder <kashik.roman@rambler.ru>
2021-11-30 18:21:11 +0300

    ListView presentation template

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -3,6 +3,7 @@
 /* @var $this yii\web\View */
 
 /* @var $dataProvider  \frontend\controllers\SiteController */
+
 /* @var $searchModel  \frontend\controllers\SiteController */
 
 use common\models\User;
@@ -16,99 +17,52 @@ use yii\widgets\Pjax;
 
 <?php
 Pjax::begin(); ?>
-<!--	<div class="row">-->
-<!--		<div class="col">-->
-<!--			--><?php // $headers = Yii::$app->request->headers;
-//            echo $accept = $headers->get('Accept');
-//             ;?>
-<!---->
-<!--		</div>-->
-<!--	</div>-->
-	<div class="site-index" >
-		<div class="body-content">
-			<div class="row">
-				<div class="col-xl-2">
+
+    <div class="site-index">
+        <div class="body-content">
+            <div class="row">
+                <div class="col-xl-2">
                     <?php
                     echo $this->render('@common/modules/blog/views/post/_search', ['model' => $searchModel]);
                     ?>
-				</div>
-				<div class="col-xl-8" id="pjax-container">
-					<div class="row ">
-                        <div class="col">
-                            <?=
-                            ListView::widget([
-                                'dataProvider' => $dataProvider,
-                            ]);
-                            ?>
+                </div>
+                <div class="col-xl-8" id="pjax-container">
+                    <div class="row ">
+                        <?=
+                        ListView::widget([
+                            'dataProvider' => $dataProvider,
+                            'options' => [
+                                'tag' => false,
+                                #'class' => 'col-12 col-lg-6 mb-3 list-wrapper',
+                                #'#id' => 'list-wrapper',
+                            ],
+                            'layout' => "{pager}\n{items}\n{summary}", //TODO исправить шаблон отображения
+                            'itemOptions' => ['tag' => null],
+                            'itemView' => function ($model, $key, $index, $widget) {
 
-                        </div>
-                        <?php
-                        foreach ($dataProvider->getModels() as $key => $post) : ?>
-                            <?php
-                            if ($key % 2 !== 0) {
-                                $class = 'alt';
-                            } else {
-                                $class = '';
-                            } ?>
-							<div class="col-12 col-lg-6 mb-3">
-								<div class="blog-card mx-auto <?php
-                                echo $class; ?>"
-									 id="<?php
-                                     echo $post['id']; ?>">
-									<div class="meta">
-										<div class="photo"
-											 style="background-image: url(<?php
-                                             echo Yii::$app->storage->getFile($post['preview']) ?>)"></div>
-										<ul class="details">
-											<li class="author">
-                                                <?php
-                                                echo Html::a(
-                                                    $post['user']['username'],
-                                                    ['/site/view', 'id' => $post['user_id']],
-                                                ); ?>
+                                return $this->render('_post_item',
+                                    [
+                                        'model' => $model,
+                                        'index' => $index,
+                                        'key' => $key
+                                    ]);
 
-											</li>
-											<li class="tags">
-												<i class="fas fa-tag  mr-2"></i>
-                                                <?= $post['category']['category_name']; ?>
-											</li>
+                                // or just do some echo
+                                // return $model->title . ' posted by ' . $model->author;
+                            },
+                        ]);
+                        ?>
 
-											<li class="date"><?php
-                                                echo date('d/m/y', $post['created_at']); ?></li>
-										</ul>
-									</div>
-									<div class="description">
-										<h1><?php
-                                            echo Html::a($post['title'], ['/blog/post/post', 'slug' => $post['slug']]);
-                                            ?></h1>
-										<h2><?php
-                                            echo $post['subtitle']; ?></h2>
-										<p></p>
-										<!--<div class="truncate-text no-img"> <?php
-                                        /*echo $post['subtitle'] ;*/ ?></div>-->
-										<p class="read-more">
-                                            <?php
-                                            echo Html::a(
-                                                ' Читать <i class="fas fa-arrow-right"></i>',
-                                                ['/blog/post/post', 'slug' => $post['slug']]
-                                            );
-                                            ?>
-										</p>
-									</div>
-								</div>
-							</div>
-                        <?php
-                        endforeach; ?>
-					</div>
+                    </div>
 
-				</div>
-				<div class="d-none d-xl-block col-xl-2">
+                </div>
+                <div class="d-none d-xl-block col-xl-2">
                     <?php
                     echo NewsList::widget(['showLimit' => 3]); ?>
-				</div>
-			</div>
+                </div>
+            </div>
 
-		</div>
-	</div>
+        </div>
+    </div>
 <?php
 Pjax::end(); ?>
